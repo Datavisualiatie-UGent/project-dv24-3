0e9e19c2b214a2145e797a1a3b898c0fe587b041
robbe0x00 <100378224+robbe0x00@users.noreply.github.com>
2024-05-11 15:05:11 +0000

    Deploying to gh-pages from @ Datavisualiatie-UGent/project-dv24-3@bd45de2857413dedf014f3ffd4d7ffd4a0a0e683 🚀

---
+++ _import/components/legends.61a35010.js
@@ -0,0 +1,45 @@
+import * as d3 from "../../_npm/d3@7.9.0/_esm.js";
+
+export function color_mapping (distinct, colors) {
+    return Object.fromEntries(distinct.map((x, index) => [x, colors[index % colors.length]]));
+}
+
+export function add_color_to_data(data, colors_mapped, key) {
+    return data.map(d => ({ ...d, color: colors_mapped[d[key]] }));
+}
+
+export function legend(colors) {
+    const distinct = Object.keys(colors);
+    const padding = 10; // Padding between legend items
+    const lineHeight = 20; // Height of each line in the legend
+    const fontSize = 12; // Fixed font size
+    const svgWidth = 200;
+    const svgHeight = 600;
+
+    const svg = d3.create("svg")
+        .attr("viewBox", [0, 0, svgWidth, svgHeight])
+        .style("font", `${fontSize}px sans-serif`)
+        .style("user-select", "none");
+  
+    const legend = svg.append("g")
+        .selectAll("g")
+        .data(distinct)
+        .join("g")
+        .attr("transform", (d, i) => `translate(0, ${lineHeight * i})`);
+  
+    legend.append("rect")
+        .attr("x", padding)
+        .attr("y", 5)
+        .attr("width", 10)
+        .attr("height", 10)
+        .attr("fill", d => colors[d]);
+  
+    legend.append("text")
+        .attr("x", padding + 15)
+        .attr("y", 10)
+        .attr("dy", "0.35em")
+        .text(d => d);
+  
+    return svg.node();
+}
+
